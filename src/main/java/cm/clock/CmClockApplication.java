e78ad28664d2b0d117eb4d46f79fa4cb5886e79d
Yhh2280 <Yhh2280@noreply.gitcode.com>
2024-11-04 16:38:32 +0800

    项目初始化

---
+++ src/main/java/cm/clock/CmClockApplication.java
@@ -0,0 +1,13 @@
+package cm.clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cm/clock/CmClockApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
